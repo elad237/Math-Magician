d2b0a17052bcd186fd84a22edd57135213acef83
elad237 <tumbu.elad@gmail.com>
2023-02-17 01:11:37 +0100

    Added the Output.js file

---
+++ src/components/Output.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Output extends React.PureComponent {
+  render() {
+    return (
+      <div className="output">
+        0
+      </div>
+    );
+  }
+}
+
+export default Output;
